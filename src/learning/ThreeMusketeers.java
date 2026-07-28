a9b4aaf01a9efb4c0fef1542490f4ec0e58cbb12
jwn7 <ksyzing@naver.com>
2026-07-29 08:52:17 +0900

    Solve ThreeMusketeers

---
+++ src/learning/ThreeMusketeers.java
@@ -0,0 +1,24 @@
+/*
+ * 날짜: 2026-07-29
+ * 문제: 삼총사
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/131705
+ */
+public class ThreeMusketeers {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for(int i = 0; i<number.length - 2; i++)
+        {
+            for(int j = i+1; j<number.length - 1; j++)
+            {
+                for(int k = j+1; k<number.length; k++)
+                {
+                    if(number[i] + number[j] + number[k] == 0)
+                        answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
